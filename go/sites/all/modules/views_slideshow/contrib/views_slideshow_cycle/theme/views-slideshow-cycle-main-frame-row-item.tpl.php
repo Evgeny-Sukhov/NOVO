fd09025d9a15b3603bfc006c55d89ae36d2544d7
evgenysukhov <sukhov@me.com>
2019-02-27 22:53:55 +0100

    Front page + Backend

---
+++ go/sites/all/modules/views_slideshow/contrib/views_slideshow_cycle/theme/views-slideshow-cycle-main-frame-row-item.tpl.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @file
+ * Views Slideshow cycle: Main frame row item.
+ *
+ * - $classes: Classes.
+ * - $item: Row item.
+ *
+ * @ingroup vss_templates
+ */
+?>
+<div class="row-item">
+  <?php print $item; ?>
+</div>
